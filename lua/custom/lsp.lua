888be305955c2c490866f21c506d7d2132d6a885
Raúl Ferràs <raulferras@gmail.com>
2024-06-19 07:39:47 +0200

    Configure LSP

--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -203,15 +203,21 @@ local setup = function()
   -- Configure c3-lsp 
   local lspconfig = require('lspconfig')
   local util = require('lspconfig/util')
-  
-  lspconfig.c3_lsp.setup {
-    name = "c3_lsp",
-    cmd = { "/Volumes/Development/raul/c3/go-lsp/server/bin/c3-lsp" },
-    filetypes = { "c3", "c3i" },
-    root_dir = function(fname)
-      return util.find_git_ancestor(fname)
-    end,
-  }
+  local configs = require('lspconfig.configs')
+  if not configs.c3_lsp then
+	  configs.c3_lsp = {
+	  	default_config = {
+	  		cmd = { "/Volumes/Development/raul/c3/go-lsp/server/bin/c3-lsp" },
+	  		filetypes = { "c3", "c3i" },
+	  		root_dir = function(fname)
+	      		return util.find_git_ancestor(fname)
+	    	end,
+	    	settings = {},
+	    	name = "c3_lsp"
+	  	}
+	  }
+  end
+  lspconfig.c3_lsp.setup{}
 end;
 
 
